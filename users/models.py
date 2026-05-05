6eedde81e9d283eab02a2e5f155fbca69748cfac
Muhammad Hamid Rehman <hamidrehman1012@gmail.com>
2026-05-05 14:11:59 +0500

    add users

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# User model.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
